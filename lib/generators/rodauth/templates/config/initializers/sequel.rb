e50647f5d2a1ba0e9496b50f190da96a1cede424
Janko Marohnić <janko.marohnic@gmail.com>
2020-07-26 20:44:41 +0200

    Test on JRuby

--- lib/generators/rodauth/templates/config/initializers/sequel.rb
+++ lib/generators/rodauth/templates/config/initializers/sequel.rb
@@ -1,6 +1,10 @@
 require "sequel/core"
 
 # initialize the appropriate Sequel adapter without creating a connection
+<%- if RUBY_ENGINE == "jruby" -%>
+DB = Sequel.connect("jdbc:<%= sequel_adapter %>://", test: false)
+<% else -%>
 DB = Sequel.<%= sequel_adapter %>(test: false)
+<% end -%>
 # have Sequel use ActiveRecord's connection for database interaction
 DB.extension :activerecord_connection
