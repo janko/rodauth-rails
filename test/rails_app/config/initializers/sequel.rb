757c9819820183b68ad9252937bba51d0fd25075
Janko Marohnić <janko.marohnic@gmail.com>
2020-11-08 14:51:54 +0100

    Remove `test: false` in sequel.rb initializer
    sequel-activerecord_connection 1.1+ disables testing the connection by
    default, so we don't need to explicitly disable it ourselves anymore.

--- test/rails_app/config/initializers/sequel.rb
+++ test/rails_app/config/initializers/sequel.rb
@@ -1,3 +1,3 @@
 require "sequel/core"
 
-DB = Sequel.connect("#{"jdbc:" if RUBY_ENGINE == "jruby"}sqlite://", extensions: :activerecord_connection, test: false)
+DB = Sequel.connect("#{"jdbc:" if RUBY_ENGINE == "jruby"}sqlite://", extensions: :activerecord_connection)
