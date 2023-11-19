1d41cfb514cf1694c0f113799b6e0057e0f544a9
Janko Marohnić <janko.marohnic@gmail.com>
2023-11-19 18:26:46 +0100

    Load Action Cable in tests
    It seems that eager loading turbo-rails on Rails 7.1 depends on Action
    Cable being loaded.

--- test/rails_app/config/application.rb
+++ test/rails_app/config/application.rb
@@ -1,6 +1,7 @@
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_cable/engine"
 require "active_job/railtie"
 require "rails/test_unit/railtie"
 
