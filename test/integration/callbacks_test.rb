3a77202816766a76644b505701c107a2be286cc7
Janko Marohnić <janko.marohnic@gmail.com>
2023-11-26 09:50:48 +0100

    Enable per-action controller callbacks

--- test/integration/callbacks_test.rb
+++ test/integration/callbacks_test.rb
@@ -5,18 +5,26 @@ class CallbacksTest < IntegrationTest
     visit "/login"
 
     assert_match "login-form", page.html
-    assert_equal 200,          page.status_code
-    assert_equal "true",       page.response_headers["X-Before-Action"]
-    assert_equal "true",       page.response_headers["X-After-Action"]
-    assert_equal "true",       page.response_headers["X-Before-Around-Action"]
-    assert_equal "true",       page.response_headers["X-After-Around-Action"]
+    assert_equal 200, page.status_code
+    assert_equal "true", page.response_headers["X-Before-Action"]
+    assert_equal "true", page.response_headers["X-After-Action"]
+    assert_equal "true", page.response_headers["X-Before-Around-Action"]
+    assert_equal "true", page.response_headers["X-After-Around-Action"]
+  end
+
+  test "runs callbacks for specific actions" do
+    visit "/create-account"
+    assert_equal "true", page.response_headers["X-Before-Specific-Action"]
+
+    visit "/login"
+    assert_nil page.response_headers["X-Before-Specific-Action"]
   end
 
   test "handles rendering in callback chain" do
     visit "/login?early_return=true&fail=true"
 
     assert_equal "early return", page.html
-    assert_equal 201,            page.status_code
-    assert_equal "true",         page.response_headers["X-Before-Action"]
+    assert_equal 201, page.status_code
+    assert_equal "true", page.response_headers["X-Before-Action"]
   end
 end
