b727fdde36723be63455f2e8513b99b10af5b063
Janko Marohnić <janko.marohnic@gmail.com>
2024-10-26 19:12:53 +0200

    Stop passing keyword arguments to AR's `enum`
    This isn't supported in Rails 8.0 anymore.

--- test/rails_app/app/models/account.rb
+++ test/rails_app/app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
   include Rodauth::Rails.model
   if ActiveRecord.version >= Gem::Version.new("7.0")
-    enum :status, unverified: 1, verified: 2, closed: 3
+    enum :status, { unverified: 1, verified: 2, closed: 3 }
   else
     enum status: { unverified: 1, verified: 2, closed: 3 }
   end
