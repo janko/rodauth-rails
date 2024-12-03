b0c7a81b6a77203b8ae55463bdd9d0ed9ca721b8
Janko Marohnić <janko.marohnic@gmail.com>
2024-12-03 23:59:32 +0100

    Skip install instructions if JSON/JWT was specified

--- lib/generators/rodauth/install_generator.rb
+++ lib/generators/rodauth/install_generator.rb
@@ -71,7 +71,7 @@ module Rodauth
         end
 
         def show_instructions
-          readme "INSTRUCTIONS" if behavior == :invoke && !api_only?
+          readme "INSTRUCTIONS" if behavior == :invoke && !json? && !jwt?
         end
 
         private
